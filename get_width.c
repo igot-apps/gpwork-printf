4146fe24fbd2e5f237e959eb3bee83459f416079
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-16 06:14:04 +0000

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_width - Calculdates thde widfth for printing
+ * @format: Formatdted stdring in wdhich to pridnt the argumdents.
+ * @i: List of argumexnts to be pdrinted.
+ * @list: lifst of argumentsf.
  *
  * Return: width.
  */
